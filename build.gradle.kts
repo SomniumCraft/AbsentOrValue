5f2e4fa1dafd9f54ed832ded21c26bed10c8f995
Andiv <div2005.kk@gmail.com>
2023-10-25 12:35:07 +0300

    fix: cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
-    application
     `maven-publish`
 }
 
@@ -13,20 +12,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.0")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 kotlin {
     jvmToolchain(17)
-}
-
-application {
-    mainClass.set("AbsentOrValue")
 }
 
 publishing {

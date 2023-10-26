304867c1a7b81ca2e1e34ae7f9900abfb3abe30b
VitSun666 <61446535+VitSun666@users.noreply.github.com>
2023-10-26 17:39:03 +0300

    build: version bump, testing multiplatform maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.scmc"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
     mavenCentral()
@@ -73,14 +73,6 @@ publishing {
             authentication {
                 create<BasicAuthentication>("basic")
             }
-        }
-    }
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = "absentorvalue"
-            version = project.version.toString()
-            from(components["java"])
         }
     }
 }
